f287e0e4e04c40d9d09009555f91273b90500423
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-04-05 16:18:27 -0300

    Create new Pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,18 @@ class PicsController < ApplicationController
 
   end
 
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title,:description)
+  end
+
 end
